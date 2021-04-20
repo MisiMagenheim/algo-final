c1d1bfdd69e33c708e0a55d7c09b8f2f4ddeb702
Misi <misi.magenheim@gmail.com>
2021-04-20 14:45:14 +0200

    elso pipa

--- src/main/java/com/codecool/whattoprint/Challenge.java
+++ src/main/java/com/codecool/whattoprint/Challenge.java
@@ -3,6 +3,15 @@ package com.codecool.whattoprint;
 class Challenge {
     public static String decideAndSay(String[] words, int number) {
         //TODO your code comes here
-        return null;
+        if (number % 3 == 0 && number % 7 == 0) {
+            return words[0] + words[1];
+        }
+        else if (number % 3 == 0) {
+            return words[0];
+        }
+        else if (number % 7 == 0) {
+            return words[1];
+        }
+        return "";
     }
 }
